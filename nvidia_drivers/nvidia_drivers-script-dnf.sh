ad4d2c30488280c1b57ed79b2ccd5cb2d50b3680
Mateus-jonathas2033 <jonathasa.2033@gmail.com>
2026-04-20 11:29:25 -0300

    Nvidia drivers installation script for dnf 'n rpm-ostree

---
+++ nvidia_drivers/nvidia_drivers-script-dnf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COLOR="\033[32m"
+NC="\033[0m"
+
+echo -e "${COLOR}\nFirstly, let's install RPMfusion, which is an important directory for Fedora, for those who don't want to do a lot of manual work.\nIt is reviewed and worked on by some devs who officially code Fedora.\n${NC}"
+sudo dnf install \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+echo -e "${COLOR}\nNow, we'll install Nvidia Akmod, it's official, and Nvidia Cuda and Vulkan tools.\n${NC}"
+sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda vulkan-loader vulkan-tools
+
+Nvidiainformation=$(modinfo -F version nvidia)
+ptyxis --tab -- bash -c 'echo -ne "\033]0;NVIDIA GeForce Driver: '${Nvidiainformation}'\007"; watch -n 1 nvidia-smi'
+echo -e "${COLOR}Vulkan drivers version...${NC}"; vulkaninfo 2>&1| grep -iEm  2 'GPU ID = 0|VULKAN INSTANCE VERSION' | grep -v WARNING
+echo -e "${COLOR}\nReboot your PC after 30 minutes, cause after the installation, the drivers will start the compilation process.${NC}"
+ 
