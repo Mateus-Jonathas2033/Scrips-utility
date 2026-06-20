d00b8a4b93cfe244fc53a0dfd3bebf90edb350f1
Mateus-jonathas2033 <jonathasa.2033@gmail.com>
2026-06-19 21:23:13 -0300

    Feature: autoremove to update-script

--- update/update-script-apt.sh
+++ update/update-script-apt.sh
@@ -6,5 +6,6 @@ OUT="\033[0m"
 echo -e "${COLOR}Searching...${OUT}"
 sudo apt update; echo -e "${COLOR}\nList of programs upgradable:${OUT}"
 apt list --upgradable; echo "" 
-sudo apt upgrade -y 
+sudo apt upgrade -y
+sudo apt autoremove -y
 echo -e "${COLOR}Finished!${OUT}"
--- update/update-script-dnf.sh
+++ update/update-script-dnf.sh
@@ -5,4 +5,5 @@ OUT="\033[0m"
 
 echo -e "${COLOR}Searching...${OUT}"
 sudo dnf update -y --refresh
+sudo dnf autoremove -y
 echo -e "${COLOR}Finished!${OUT}"
